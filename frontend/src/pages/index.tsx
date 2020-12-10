315e4449aa7bc5235e13ee37c1e4c46a004d47d2
jonasdevzero <jonasdevzero@gmail.com>
2020-12-09 21:02:04 -0300

    feat: config frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>ZeroHard</title>
+      </Head>
+
+      <body>
+        <h1>Zero Hard</h1>
+      </body>
+
+    </div>
+  );
+}
