09a2f42159ee6c28be941db31f471c4d960f8fb9
jonasdevzero <jonasdevzero@gmail.com>
2020-12-16 22:26:08 -0300

    feat

--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-import usePersistedState from '../hooks/usePersistedState'
+import api from '../services/api';
+import usePersistedState from '../hooks/usePersistedState';
 
 import { Main } from '../styles/pages/Global';
 import {
@@ -9,28 +11,66 @@ import {
 } from '../styles/pages/Signin';
 import {
     Form,
-    Input
+    Input,
+    Label,
+    Wrapper,
+    Submit,
+    Error,
 } from '../styles/components/Form';
 
 export default function Signin() {
-    const [token, setToken] = usePersistedState('token', "")
+    const [token, setToken] = usePersistedState('token', "");
+    const router = useRouter();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState("");
 
     useEffect(() => {
         if (token === "") return;
 
-        
-    }, [])
+        // api.post("/user/auth", { token })
+        //     .then(({ data }) => {
+        //         if (data.auth) return router.push("/app");
+        //     });
+    }, []);
+
+    async function login(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        await api.post("/user/login", { email, password })
+            .then(({ data }) => {
+                setToken(data.token);
+                router.push("/app");
+            })
+            .catch(error => setError(error.message));
+    };
 
     return (
         <>
             <Head>
-
+                <title>TaskEasy | SignIn</title>
             </Head>
 
             <Main>
                 <FormContainer>
-                    <Form>
-                        <Input type="text" />
+                    <Form onSubmit={login}>
+                        {error.length > 0 ? (
+                            <Error>{error}</Error>
+                        ) : null}
+
+                        <Wrapper>
+                            <Label>email</Label>
+                            <Input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+                        </Wrapper>
+
+                        <Wrapper>
+                            <Label>password</Label>
+                            <Input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                        </Wrapper>
+
+                        <Submit type="submit">SignIn</Submit>
                     </Form>
                 </FormContainer>
             </Main>
--- frontend/src/styles/components/Form.ts
+++ frontend/src/styles/components/Form.ts
@@ -6,8 +6,39 @@ export const Form = styled.form`
 
     display: flex;
     flex-direction: column;
+
+    background-color: #fff;
+    padding: 5rem 4rem;
 `;
 
 export const Input = styled.input`
+    width: 30rem;
+    height: 4rem;
+
+    border: solid .1rem #000;
+    outline: none;
+    padding: 0 1rem;
+`;
+
+export const Label = styled.label`
+    color: #000;
+    font-size: 1.5rem;
+    margin-bottom: .5rem;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 1rem 0;
+`;
+
+export const Submit = styled.button`
+    height: 4rem;
+    margin-top: 2rem;
+
+    cursor: pointer;
+`;
+
+export const Error = styled.strong`
 
 `;
