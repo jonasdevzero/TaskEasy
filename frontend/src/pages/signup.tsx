f4c967f3fe627b24da5fac0b5095ee74b2da2092
jonasdevzero <jonasdevzero@gmail.com>
2020-12-16 22:34:19 -0300

    feat

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,18 +1,68 @@
+import { useState } from "react";
 import Head from 'next/head';
+
+import api from "../services/api";
 
 import {
     Main,
 } from '../styles/pages/Global';
+import {
+    Form,
+    Input,
+    Label,
+    Wrapper,
+    Submit,
+    Error,
+} from '../styles/components/Form';
 
 export default function Signup() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState("");
+
+    async function create(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        await api.post("user", { name, email, password })
+            .then(response => {
+
+            })
+            .catch(error => setError(error.message));
+    };
+
     return (
         <>
             <Head>
-
+                <title>TaskEasy | SignUp</title>
             </Head>
 
             <Main>
+                <div>
+                    <Form onSubmit={create}>
+                        {error.length > 0 ? (
+                            <Error>{error}</Error>
+                        ) : null}
 
+                        <Wrapper>
+                            <Label>name</Label>
+                            <Input type="text" value={name} onChange={e => setName(e.target.value)} />
+                        </Wrapper>
+
+                        <Wrapper>
+                            <Label>email</Label>
+                            <Input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+                        </Wrapper>
+
+                        <Wrapper>
+                            <Label>password</Label>
+                            <Input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                        </Wrapper>
+
+                        <Submit type="submit">SignUp</Submit>
+                    </Form>
+                </div>
             </Main>
         </>
     );
