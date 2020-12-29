e209ec10c08cf74d307ca0f3d7b341445ee66bec
jonasdevzero <jonasdevzero@gmail.com>
2020-12-28 21:37:22 -0300

    fix

--- backend/src/utils/date.ts
+++ backend/src/utils/date.ts
@@ -1,10 +1,17 @@
 export function getTime() {
-    const day = new Date().getDay();
-    const month = new Date().getMonth();
-    const year = new Date().getFullYear();
-    const hour = new Date().getHours();
-    const minutes = new Date().getMinutes();
-    const seconds = new Date().getSeconds();
+    const date = new Date();
 
-    return `${year}/${month}/${day} ${hour}:${minutes}:${seconds}`;
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    let hours = date.getHours().toString();
+    let minutes = date.getMinutes().toString();
+    let seconds = date.getSeconds().toString();
+
+    Number(hours) < 10 ? hours = "0".concat(hours) : hours;
+    Number(minutes) < 10 ? minutes = "0".concat(minutes) : minutes;
+    Number(seconds) < 10 ? seconds = "0".concat(seconds) : seconds;
+
+    return `${month + 1}/${day}/${year}T${hours}:${minutes}:${seconds}`;
 };
